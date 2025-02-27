ba367bf8c0cd8479837f6ff73130b1d9645817e0
mtkatsaros <mtkatsaros@gmail.com>
2025-02-27 02:17:42 -0800

    Finish chapter 2

---
+++ ch2/lower.c
@@ -0,0 +1,21 @@
+/**~~~~~~~~~~~~~~~lower.c~~~~~~~~~~~~~~~~~~~
+ * Converts string to all lower case values.
+ *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+
+ #include <stdio.h>
+
+ // Either returns the difference between lower and uppercase ascii added to the 
+ // character value if it's uppercase, or just the character itself otherwise
+ char lower(char c){
+    return (c >= 'A' && c <= 'Z') ? (c + ('a' - 'A')) : c;
+ }
+
+ int main(){
+    printf("Enter a character: ");
+    char c = getchar();
+
+    printf("\nResult: %c\n", lower(c));
+
+    return 0;
+ }
